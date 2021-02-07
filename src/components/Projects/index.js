1493b27bc837c0bf8dee61872f19c997acbe16c4
Pere Grinus <gregjleighton@gmail.com>
2021-02-07 18:01:05 -0500

    added some dummy cards to Projects/index.js, just to have content on the page.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -16,7 +16,54 @@ function Projects() {
         <section style={styles.containerStyle} className="container">
             <p>
                 Here you can check out some of the projects I've made throughout the journey of honing my skills as a full-stack developer! 
+                
             </p>
+
+            <div className="card">
+  <div class="card-body">
+    <h5 class="card-title">Plant-Scout</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+<div className="card">
+  <div class="card-body">
+    <h5 class="card-title">Plant-Scout</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+<div className="card">
+  <div class="card-body">
+    <h5 class="card-title">Plant-Scout</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+<div className="card">
+  <div class="card-body">
+    <h5 class="card-title">Plant-Scout</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+<div className="card">
+  <div class="card-body">
+    <h5 class="card-title">Plant-Scout</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
 
         </section>
 
