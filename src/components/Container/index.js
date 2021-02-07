da9ab62ca43002db55c3199c1382db6ed964d580
Pere Grinus <gregjleighton@gmail.com>
2021-02-07 16:11:04 -0500

    fixed small error in Container / index.js

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -10,16 +10,16 @@ const styles = {
     }
 }
 
-function Header() {
+function Container() {
     return (
         <section style={styles.containerStyle} className="container">
             <p>
                 My name is Greg Leighton. In addition to being a professional historian of the Middle Ages, I am also a Web Developer. One of the things that I enjoy most is learning new skills, making new contacts, and building exciting, engaging, and accessible tools for people. I'm currently a certification in Full Stack Web Development from the University of Pennsylvania. When I am not coding, I love to hike, to read, (try) to run, and to fish. Looking for a new project or application that you'd like to see built? Feel free to get in touch with me! 
             </p>
-            
+
         </section>
 
     );
 }
 
-export default Header;
+export default Container;
