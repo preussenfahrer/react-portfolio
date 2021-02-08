1cda3bdc9274bbcd94ee0995f9ab9b519838de3b
Pere Grinus <gregjleighton@gmail.com>
2021-02-08 09:04:04 -0500

    updated links and descriptions for my 6 projects. Still want to get a more interactive user experience by including photos. Links are working and page flows smoothly. Will now add some more styling to each page so that it is not just plain bootstrap.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -15,53 +15,53 @@ function Projects() {
     return (
         <section style={styles.containerStyle} className="container">
             <p>
-                Here you can check out some of the projects I've made throughout the journey of honing my skills as a full-stack developer! 
+                Here you can check out some of the projects I've made throughout the journey of honing my skills as a full-stack developer! Some of these apps are live, but others are just to demonstrate the skills I've learned. Feel free to check out the repos (and, if you want, fork them), live links, and video walk demos.
                 
             </p>
 
             <div className="card">
-  <div class="card-body">
-    <h5 class="card-title">Plant-Scout</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+  <div className="card-body">
+    <h5 className="card-title">Plant-Scout</h5>
+    <h6 className="card-subtitle mb-2 text-muted"><i>Let's go for a walk</i></h6>
+    <p className="card-text">PlantScout is a JavaScript based application that allows you to explore local plantlife in your region.</p>
+    <a href="https://github.com/preussenfahrer/plant-scout" className="card-link">GitHub Repo</a>
+    <a href="https://karajsch.github.io/plant-scout/" className="card-link">Deployed Application</a>
   </div>
 </div>
 <div className="card">
-  <div class="card-body">
-    <h5 class="card-title">Plant-Scout</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+  <div className="card-body">
+    <h5 className="card-title">Shop-n-Chef</h5>
+    <h6 className="card-subtitle mb-2 text-muted"><i>Turn your grocery list into a passport for new adventures!</i></h6>
+    <p className="card-text">Shop-n-Chef is a disposable grocery shopping application where you can not just search for the groceries you need, but also get recipes on how to use them.</p>
+    <a href="https://github.com/preussenfahrer/shop_n_chef" className="card-link">GitHub Repo</a>
+    <a href="https://shop-n-chef.herokuapp.com/" className="card-link">Deployed Application</a>
   </div>
 </div>
 <div className="card">
-  <div class="card-body">
-    <h5 class="card-title">Plant-Scout</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+  <div className="card-body">
+    <h5 className="card-title">PWA Budget Tracker</h5>
+    <h6 className="card-subtitle mb-2 text-muted"><i>A budget is telling your money where to go, instead of asking where it went.</i></h6>
+    <p className="card-text">Budget-Tracker allows you to add and subtract expenses and monitor your spending, from anywhere, on any device.</p>
+    <a href="https://github.com/preussenfahrer/Budget-Tracker" className="card-link">GitHub Repo</a>
+    <a href="#https://morning-garden-03578.herokuapp.com/" className="card-link">Deployed Application</a>
   </div>
 </div>
 <div className="card">
-  <div class="card-body">
-    <h5 class="card-title">Plant-Scout</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+  <div className="card-body">
+    <h5 className="card-title">Employee Tracker</h5>
+    <h6 className="card-subtitle mb-2 text-muted"><i>Build a functional and easy to use directory of employees for your business</i></h6>
+    <p className="card-text">Keep track of the employees for your company with this CLI.</p>
+    <a href="https://github.com/preussenfahrer/EmployeeTracker" className="card-link">GitHub Repo</a>
+    <a href="https://drive.google.com/file/d/1N5VLuZFXxTiVJvBSZp0YQRpF8TYUGmy_/view" className="card-link">Video Demo</a>
   </div>
 </div>
 <div className="card">
-  <div class="card-body">
-    <h5 class="card-title">Plant-Scout</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+  <div className="card-body">
+    <h5 className="card-title">Note-Taker</h5>
+    <h6 className="card-subtitle mb-2 text-muted"><i>Record your thoughts, tasks or ideas, and reflect on them.</i></h6>
+    <p className="card-text">Note-Taker is a simple app where you can make a list of things to do, or jot down random ideas you have throughout your day.</p>
+    <a href="https://github.com/preussenfahrer/notetakerapp" className="card-link">GitHub Repo</a>
+    <a href="https://drive.google.com/file/d/1oCOtYvZiA653tQ28nJqmp-Obo0222_qJ/view" className="card-link">Video Demo</a>
   </div>
 </div>
 
