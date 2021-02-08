7a32be3110e7a6a8c574b8bab602bb5f6065ec6c
Pere Grinus <gregjleighton@gmail.com>
2021-02-08 16:24:52 -0500

    added jumbotron for about me in Container/index.js in addition to special characters for resume. Also styled header in Header/index.js.

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -7,23 +7,22 @@ const styles = {
         background: "transparent"
     },
     containerFont: {
-        fontSize: 100
+        fontSize: 22
     }
 }
 
 function Container() {
     return (
-        <section style={styles.containerStyle} className="container">
-            <p>
-                Hi! My name is Greg. In addition to being a professional historian of the Middle Ages, I am also a Web Developer. One of the things that I enjoy most is learning new skills, making new contacts, and building exciting, engaging, and accessible tools for people. I'm currently pursuing a certification in Full Stack Web Development from the University of Pennsylvania. When I am not coding, I love to hike, to read, (try) to run, and to fish. Looking for a new project or application that you'd like to see built? Feel free to get in touch with me! 
+        <section style={styles.containerStyle} className="jumbotron justify-content-center bg-dark w-75 p-3">
+            <p style={styles.containerFont}>
+                Hi, there! Thank you for taking the time to look at my portfolio. I'm a professional historian of the Middle Ages and aspiring Web Developer. Learning new skills, building new bridges, and creating exciting, engaging, and accessible tools for people are what drives me, personally and professionally. When I am not coding or writing, I hike, read, (try) to run, and fish. Looking for a new project or application that you'd like to see built? Feel free to get in touch with me! 
             </p>
             <div className="d-flex flex-row flex-wrap justify-content-center">
-    <a className="px-2" href={CV} download>CV</a>
-    <a className="px-2" href="mailto:gregjleighton@gmail.com">Email</a>
-    <a className="px-2" href="https://github.com/preussenfahrer">GitHub</a>
-    <a className="px-2" href="https://www.linkedin.com/in/greg-leighton-59b411142/">LinkedIn</a>
-    </div>
-
+            <a className="px-2" href={CV} download>R&eacute;sum&eacute;</a>
+            <a className="px-2" href="mailto:gregjleighton@gmail.com">Email</a>
+            <a className="px-2" href="https://github.com/preussenfahrer">GitHub</a>
+            <a className="px-2" href="https://www.linkedin.com/in/greg-leighton-59b411142/">LinkedIn</a>
+            </div>
         </section>
 
     );
