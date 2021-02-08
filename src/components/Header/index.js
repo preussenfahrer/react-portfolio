dbcc0de2f85433fd5940655e67213a1a4e1ad76e
Pere Grinus <gregjleighton@gmail.com>
2021-02-08 09:19:04 -0500

    added target to links in Portfolio/index.js so that repos, live apps, and video demos open in separate tabs.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -16,20 +16,20 @@ function Header() {
             <h1 class="card-header" style={styles.headerStyle}>I'm Greg Leighton</h1>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand"><i>My portfolio</i></a>
+                    <a class="navbar-brand">My portfolio</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                                <a class="nav-link active" aria-current="page" href="/"><i>About Me</i></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/Projects">Projects</a>
+                                <a class="nav-link" href="/Projects"><i>Project Portfolio</i></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Qualifications and CV</a>
+                                <a class="nav-link" href="#"><i>Qualifications & CV</i></a>
                             </li>
                         </ul>
                     </div>
