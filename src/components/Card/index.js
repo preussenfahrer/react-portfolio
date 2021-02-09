1e50bcdc1db65ea7da7fdf607196482646897c36
Pere Grinus <gregjleighton@gmail.com>
2021-02-09 16:51:52 -0500

    got links for each project to display in Projects/index.js. Still having some issues with the images, but my main goal right now is to get the links live and more appealing.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 
+
 function Card(props) {
     return (
 <div className="card">
             <div className="card-body">
-            <h5 className="card-title">{props.title}</h5>
-            <img alt={props.name} src={props.image}></img>
+            <h5 className="card-title">{props.name}</h5>
+            <div className="img-container">
+            <img src={props.image} />
+            </div>
             <h6 className="card-subtitle mb-2 text-muted"><i>{props.subtitle}</i></h6>
             <p className="card-text">{props.description}</p>
+            <p className="card-text">{props.github}</p>
+            <p className="card-text">{props.link}</p>
             </div>
             </div>
     );
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "../Card/index";
 import projects from "../../projects.json"
 import "./style.css";
+// import PlantScoutScreenshot from "../../utils/PlantScoutScreenshot.png"
 
 const styles = {
     containerStyle: {
@@ -22,32 +23,45 @@ function Projects() {
           <Card 
           name={projects[0].name}
           subtitle={projects[0].subtitle}
+        //   image= {PlantScoutScreenshot}
           description={projects[0].description}
+          github={projects[0].github}
+          link={projects[0].link}
           />
           <Card 
           name={projects[1].name}
           subtitle={projects[1].subtitle}
           description={projects[1].description}
+          github={projects[1].github}
+          link={projects[1].link}
           />
           <Card 
           name={projects[2].name}
           subtitle={projects[2].subtitle}
           description={projects[2].description}
+          github={projects[2].github}
+          link={projects[2].link}
           />
           <Card 
           name={projects[3].name}
           subtitle={projects[3].subtitle}
           description={projects[3].description}
+          github={projects[3].github}
+          link={projects[3].link}
           />
           <Card 
           name={projects[4].name}
           subtitle={projects[4].subtitle}
           description={projects[4].description}
+          github={projects[4].github}
+          link={projects[4].link}
           />
           <Card 
           name={projects[5].name}
           subtitle={projects[5].subtitle}
           description={projects[5].description}
+          github={projects[5].github}
+          link={projects[5].link}
           />
         </section>
       );
