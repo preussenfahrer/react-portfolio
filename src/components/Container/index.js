7eecfd86da41b2e72c8003e5d8b3f7cb392e4a9f
Pere Grinus <gregjleighton@gmail.com>
2021-02-09 18:40:31 -0500

    added dummy profile picture to homepage, but will be refactoring this and getting a more responsive one.

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CV from "../../utils/WebDevResume.pdf";
-// import picture from "../../utils/Profile_pic.jpg"
-// import "./style.css"
+import picture from "../../utils/Profile_pic.jpg"
+import "./style.css"
 
 const styles = {
     containerStyle: {
@@ -14,11 +14,11 @@ const styles = {
 
 function Container() {
     return (
-        <section style={styles.containerStyle} className="jumbotron col-lg-6 col-md-6 col-sm-6 col-xs-6 offset-3 float-sm-center bg-dark">
+        <section style={styles.containerStyle} className="jumbotron text-center col-lg-6 col-md-6 col-sm-6 col-xs-3 offset-3 float-md-center bg-dark">
             <p style={styles.containerFont}>
-            {/* <img className="img-fluid img-thumbnail 50x50" src={picture} alt="responsive"></img> */}
                 Hi, there! Thank you for taking the time to look at my portfolio. I'm a professional historian of the Middle Ages and an aspiring Web Developer. Learning new skills, building new bridges, and creating exciting tools are what drives me, personally and professionally. When I am not coding or writing, I hike, read, (try) to run, and fish. Looking for a new project or application that you'd like to see built? Feel free to get in touch with me! 
             </p>
+            <img className="img-fluid img-thumbnail 50x50" src={picture} alt="responsive"></img>
             <div className="d-flex flex-row flex-wrap justify-content-center">
             <a className="px-2" target="_blank" href={CV} download>R&eacute;sum&eacute;</a>
             <a className="px-2" target="_blank" href="mailto:gregjleighton@gmail.com">Email</a>
