86da4a55427c38a728bef5a0b1f87f22c008b761
Pere Grinus <gregjleighton@gmail.com>
2021-02-09 17:35:32 -0500

    re-styled links to one button in Projects/index.js, which will take user to the repo. Each repo has a live deployment link and video walkthrough. Still having issues rendering images in my card file.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -7,12 +7,11 @@ function Card(props) {
             <div className="card-body">
             <h5 className="card-title">{props.name}</h5>
             <div className="img-container">
-            <img src={props.image} />
+            <img alt={props.name} src={props.image} />
             </div>
             <h6 className="card-subtitle mb-2 text-muted"><i>{props.subtitle}</i></h6>
-            <p className="card-text">{props.description}</p>
-            <p className="card-text">{props.github}</p>
-            <p className="card-text">{props.link}</p>
+            <a href={props.github} target="_blank" class="btn btn-primary">Click me!</a>
+            
             </div>
             </div>
     );
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "../Card/index";
 import projects from "../../projects.json"
 import "./style.css";
-// import PlantScoutScreenshot from "../../utils/PlantScoutScreenshot.png"
+
 
 const styles = {
     containerStyle: {
@@ -17,13 +17,13 @@ function Projects() {
     return (
         <section style={styles.containerStyle} className="container">
             <p style={styles.containerFont}>
-                Here you can check out some of the projects I've made while honing my skills as a full-stack developer! Feel free to check out the repos (and, if you want, fork them), live links, and video demos.
+                Here you can check out some of the projects I've made while honing my skills as a full-stack developer! Click the buttons to find up-to-date GitHub repos, with live application deployments and video walk demos.
                 
             </p>
           <Card 
           name={projects[0].name}
           subtitle={projects[0].subtitle}
-        //   image= {PlantScoutScreenshot}
+          image={projects[0].image}
           description={projects[0].description}
           github={projects[0].github}
           link={projects[0].link}
@@ -47,21 +47,18 @@ function Projects() {
           subtitle={projects[3].subtitle}
           description={projects[3].description}
           github={projects[3].github}
-          link={projects[3].link}
           />
           <Card 
           name={projects[4].name}
           subtitle={projects[4].subtitle}
           description={projects[4].description}
           github={projects[4].github}
-          link={projects[4].link}
           />
           <Card 
           name={projects[5].name}
           subtitle={projects[5].subtitle}
           description={projects[5].description}
           github={projects[5].github}
-          link={projects[5].link}
           />
         </section>
       );
